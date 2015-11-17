f8d380241c4c9f6b4a22bb161b9221dea2351e61
Metalszorny <renden@freemail.hu>
2015-11-17 15:30:53 +0100

    Made some changed to mainobject.

--- BrickBreaker_2015/Model/MainObject.cs
+++ BrickBreaker_2015/Model/MainObject.cs
@@ -7,18 +7,45 @@ using System.Windows;
 
 namespace BrickBreaker_2015.Model
 {
+    /// <summary>
+    /// Base class for MainObject.
+    /// </summary>
     abstract class MainObject : Bindable
     {
+        #region Fields
+
+        // The area.
         protected Rect area;
+
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the area.
+        /// </summary>
+        /// <value>
+        /// The area.
+        /// </value>
         public Rect Area
         {
             get { return area; }
             set { area = value; }
         }
 
+        #endregion Properties
+
+        #region Constructors
+
         public MainObject(int posX, int posY, int width, int height)
         {
             Area = new Rect(posX, posY, width, height);
         }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #endregion Methods
     }
 }
