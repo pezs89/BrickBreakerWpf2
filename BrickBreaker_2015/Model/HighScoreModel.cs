ca2de733061975e9ec8740b7b199f1569579fdde
Metalszorny <renden@freemail.hu>
2015-11-17 12:30:22 +0100

    Made some changes on the model for the highscores.

--- BrickBreaker_2015/Model/HighScoreModel.cs
+++ BrickBreaker_2015/Model/HighScoreModel.cs
@@ -3,46 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using BrickBreaker_2015.Model;
 
 namespace BrickBreaker_2015.Model
 {
+    /// <summary>
+    /// Base class of the HighScoreModel
+    /// </summary>
     class HighScoreModel
     {
-        string playerName;
-        string playerScore;
+        #region Fields
 
+        // The player's name field of the HighScoreModel class.
+        private string playerName;
+
+        // The player's score field of the HighScoreModel class.
+        private string playerScore;
+
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the playerName.
+        /// </summary>
+        /// <value>
+        /// The playerName.
+        /// </value>
         public string PlayerName
         {
-            get
-            {
-                return playerName;
-            }
-
-            set
-            {
-                playerName = value;
-            }
+            get { return playerName; }
+            set { playerName = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the playerScore.
+        /// </summary>
+        /// <value>
+        /// The playerScore.
+        /// </value>
         public string PlayerScore
         {
-            get
-            {
-                return playerScore;
-            }
-
-            set
-            {
-                playerScore = value;
-            }
+            get { return playerScore; }
+            set { playerScore = value; }
         }
 
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighScoreModel"/> class.
+        /// </summary>
+        public HighScoreModel()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighScoreModel"/> class.
+        /// </summary>
+        /// <param name="playerName">The player's name field.</param>
+        /// <param name="playerScore">The player's score field.</param>
         public HighScoreModel(string playerName, string playerScore)
         {
-            this.playerName = playerName;
-            this.playerScore = playerScore;
+            PlayerName = playerName;
+            PlayerScore = playerScore;
         }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #endregion Methods
     }
 }
