c88bcc4f847f7a2119f8e7dbfeda46918e9d082a
Metalszorny <renden@freemail.hu>
2015-11-18 10:49:32 +0100

    Added empty model for brick game objects.

---
+++ BrickBreaker_2015/Model/Brick.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickBreaker_2015.Model
+{
+    /// <summary>
+    /// Base class for Brick.
+    /// </summary>
+    class Brick : MainObject
+    {
+        #region Fields
+
+        #endregion Fields
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Constructors
+
+        public Brick(int posX, int posY, int width, int height)
+            : base(posX, posY, width, height)
+        { }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
