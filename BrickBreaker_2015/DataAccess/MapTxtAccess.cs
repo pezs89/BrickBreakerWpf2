e835a1eb25abec408175ffa419c600580f902174
Metalszorny <renden@freemail.hu>
2015-11-19 12:10:10 +0100

    Wrote the map loading.

--- BrickBreaker_2015/DataAccess/MapTxtAccess.cs
+++ BrickBreaker_2015/DataAccess/MapTxtAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrickBreaker_2015.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,24 +15,9 @@ namespace BrickBreaker_2015.DataAccess
     {
         #region Fields
 
-        // The path to the txt file.
-        private string pathString;
-
         #endregion Fields
 
         #region Properties
-
-        /// <summary>
-        /// Gets or sets the pathString.
-        /// </summary>
-        /// <value>
-        /// The pathString.
-        /// </value>
-        private string PathString
-        {
-            get { return pathString; }
-            set { pathString = value; }
-        }
 
         #endregion Properties
 
@@ -40,15 +26,103 @@ namespace BrickBreaker_2015.DataAccess
         /// <summary>
         /// Initializes a new instance of the <see cref="MapTxtAccess"/> class.
         /// </summary>
-        /// <param name="pathstring">The path to the txt file.</param>
-        public MapTxtAccess(string pathstring)
-        {
-            PathString = pathstring;
-        }
+        public MapTxtAccess()
+        { }
 
         #endregion Constructors
 
         #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathString"></param>
+        /// <param name="brickWidth"></param>
+        /// <param name="brickHeight"></param>
+        /// <returns></returns>
+        private List<Brick> LoadMap(string pathString, double brickWidth, double brickHeight)
+        {
+            try
+            {
+                if (FileExists(pathString))
+                {
+                    double X = 0;
+                    double Y = 0;
+                    FileStream fs = new FileStream(pathString, FileMode.Open, FileAccess.Read, FileShare.None);
+                    BinaryReader rd = new BinaryReader(fs);
+
+                    while (rd.BaseStream.Position != rd.BaseStream.Length)
+                    {
+                        char[] oneLine = rd.ReadChars(24);
+
+                        for (int i = 0; i < oneLine.Length; i++)
+                        {
+                            switch (oneLine[i])
+                            {
+                                case '.':
+                                    break;
+                                case '1':
+                                    Brick brick1 = new Brick(X, Y, brickHeight, brickWidth, Brick.BricksType.Easy, @"");
+                                    brick1.ScorePoint = 10;
+                                    brick1.BreakNumber = 1;
+                                    break;
+                                case '2':
+                                    Brick brick2 = new Brick(X, Y, brickHeight, brickWidth, Brick.BricksType.Medium, @"");
+                                    brick2.ScorePoint = 20;
+                                    brick2.BreakNumber = 2;
+                                    break;
+                                case '3':
+                                    Brick brick3 = new Brick(X, Y, brickHeight, brickWidth, Brick.BricksType.Hard, @"");
+                                    brick3.ScorePoint = 30;
+                                    brick3.BreakNumber = 5;
+                                    break;
+                                case '4':
+                                    Brick brick4 = new Brick(X, Y, brickHeight, brickWidth, Brick.BricksType.Steel, @"");
+                                    brick4.ScorePoint = 40;
+                                    brick4.BreakNumber = 1;
+                                    break;
+                            }
+
+                            X += brickWidth;
+                        }
+
+                        X = 0;
+                        Y += brickHeight;
+                    }
+
+                    rd.Close();
+                    fs.Close();
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathString"></param>
+        /// <returns></returns>
+        private bool FileExists(string pathString)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(pathString) && File.Exists(pathString))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
         #endregion Methods
     }
--- BrickBreaker_2015/Model/Brick.cs
+++ BrickBreaker_2015/Model/Brick.cs
@@ -13,17 +13,98 @@ namespace BrickBreaker_2015.Model
     {
         #region Fields
 
+        //
+        private BricksType brickType;
+
+        // 
+        private int scorePoint;
+
+        // 
+        private int breakNumber;
+
+        // 
+        private string brickPicture;
+
+        // 
+        public enum BricksType
+        {
+            Easy,
+            Medium,
+            Hard,
+            Steel
+        }
+
         #endregion Fields
 
         #region Properties
+
+        /// <summary>
+        /// Gets or sets the brickType.
+        /// </summary>
+        /// <value>
+        /// The brickType.
+        /// </value>
+        public BricksType BrickType
+        {
+            get { return brickType; }
+            set { brickType = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the scorePoint.
+        /// </summary>
+        /// <value>
+        /// The scorePoint.
+        /// </value>
+        public int ScorePoint
+        {
+            get { return scorePoint; }
+            set { scorePoint = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the breakNumber.
+        /// </summary>
+        /// <value>
+        /// The breakNumber.
+        /// </value>
+        public int BreakNumber
+        {
+            get { return breakNumber; }
+            set { breakNumber = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the brickPicture.
+        /// </summary>
+        /// <value>
+        /// The brickPicture.
+        /// </value>
+        public string BrickPicture
+        {
+            get { return brickPicture; }
+            set { brickPicture = value; }
+        }
 
         #endregion Properties
 
         #region Constructors
 
-        public Brick(double posX, double posY, double width, double height)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="posX"></param>
+        /// <param name="posY"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="brickType"></param>
+        /// <param name="brickPicture"></param>
+        public Brick(double posX, double posY, double width, double height, BricksType brickType, string brickPicture)
             : base(posX, posY, width, height)
-        { }
+        {
+            BrickType = brickType;
+            BrickPicture = brickPicture;
+        }
 
         #endregion Constructors
 
